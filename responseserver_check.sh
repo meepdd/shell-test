1c3756e3c4a736f38d8c947af4f7260d88d62501
MD <101170461+meepdd@users.noreply.github.com>
2023-05-06 14:01:17 +0300

    Test №4
    Checking the server's response to the request

--- responseserver_check.sh
+++ responseserver_check.sh
@@ -1,21 +1,26 @@
-#!/bin/bash
-
+# Скачиваем 10 фотографий, которая сгенерировала нейросеть
 for i in {0..9}; do
   # Получаем ответ от сервера Random User API
-  response=$(curl -sS -w '%{http_code}' "https://randomuser.me/api/")
-  http_status=$(echo "${response: -3}")
-
-  if [[ $http_status -eq 200 ]]; then
-    # Из ответа забираем 10 фоток
-    avatar=$(echo "$response" | jq -r '.results[0].picture.large')
-    
-    # Скачиваем изображения и сохраняем их
-    if curl -sS "$avatar" > "$pictures/$i.jpg"; then
-      echo "YES"
-    else
-      echo "NO"
-    fi
-  else
+  response=$(curl -sS -w "%{http_code}" "https://randomuser.me/api/")
+  
+  # Проверяем код ответа HTTP
+  if [[ "${response: -3}" != "200" ]]; then
     echo "NO"
+    exit 1
+  fi
+  
+  # Из ответа забираем 10 фоток
+  avatar=$(echo "$response" | jq -r '.results[0].picture.large')
+  
+  # Скачиваем изображения и сохраняем их
+  curl -sS "$avatar" > "$pictures/$i.jpg"
+  
+  # Проверяем, получен ли файл
+  if [[ ! -s "$pictures/$i.jpg" ]]; then
+    echo "NO"
+    exit 1
   fi
 done
+
+# Если дошли до конца цикла, значит все файлы были получены успешно
+echo "YES"
