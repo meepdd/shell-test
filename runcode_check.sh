7f17d83aa8e75a36d60f89ae5e4b8329f427dee9
MD <101170461+meepdd@users.noreply.github.com>
2023-05-07 14:45:24 +0300

    Test №0
    Changed 0 test, so that when you check for negative tests - you can modify it

--- runcode_check.sh
+++ runcode_check.sh
@@ -1,33 +1,43 @@
 #!/bin/bash
+
 pictures="non-existen_people"
+success=true
+
 # Проверяем установлено ли утилита для обработки json из командной строки и устанавливаем если нет
 if ! command -v jq &> /dev/null; then
-        sudo apt-get update && sudo apt-get install jq || { echo "NO"; }
+    echo "jq is not installed. Installing now..."
+    sudo apt-get update && sudo apt-get install jq || success=false
 fi
 # Скачиваем утилиту которая из jpg формата в гиф
 if ! command -v convert &> /dev/null; then
-        sudo apt-get update && sudo apt-get install imagemagick || { echo "NO"; }
+    echo "imagemagick is not installed. Installing now..."
+    sudo apt-get update && sudo apt-get install imagemagick || success=false
 fi
 
 if [[ -d $pictures ]]
 then 
-    continue
+    echo "Папка для вывода найдена, продолжаем."
 else
-    mkdir -p $pictures || { echo "NO"; exit 1; }
+    echo "Папка для вывода не найдена, создаем"
+    mkdir -p $pictures || success=false
 fi
 
 # Скачиваем 10 фотографий, которая сгенерировала нейросеть
 for i in {0..9}; do
-  # Получаем ответ от сервера Random User API
-  response=$(curl -sS "https://randomuser.me/api/")
+    # Получаем ответ от сервера Random User API
+    response=$(curl -sS "https://randomuser.me/api/")
   
-  # Из ответа забираем 10 фоток
-  avatar=$(echo "$response" | jq -r '.results[0].picture.large')
+    # Из ответа забираем 10 фоток
+    avatar=$(echo "$response" | jq -r '.results[0].picture.large')
   
-  # Скачиваем изображения и сохраняем их
-  curl -sS "$avatar" > "$pictures/$i.jpg" || { echo "NO"; }
+    # Скачиваем изображения и сохраняем их
+    curl -sS "$avatar" > "$pictures/$i.jpg" || success=false
 done
 # Меняем 10 картинок в гифку 
-convert -delay 100 -loop 0 $pictures/* $pictures/users.gif || { echo "NO"; }
+convert -delay 100 -loop 0 $pictures/* $pictures/users.gif || success=false
 
-echo "OK: code works"
+if [ "$success" = true ]; then
+    echo "YES"
+else
+    echo "NO"
+fi
